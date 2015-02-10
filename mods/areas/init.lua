ed00b607687cd0100c149e2e7687c69b31adc5c7
Wuzzy <almikes@aol.com>
2015-02-10 04:07:19 +0100

    Strip areas mod down to its neccessary parts

--- mods/areas/init.lua
+++ mods/areas/init.lua
@@ -1,5 +1,6 @@
 -- Areas mod by ShadowNinja
 -- Based on node_ownership
+-- Stripped down for the tutorial
 -- License: LGPLv2+
 
 areas = {}
@@ -11,29 +12,12 @@ areas.modpath = minetest.get_modpath("areas")
 dofile(areas.modpath.."/settings.lua")
 dofile(areas.modpath.."/api.lua")
 dofile(areas.modpath.."/internal.lua")
-dofile(areas.modpath.."/chatcommands.lua")
-dofile(areas.modpath.."/pos.lua")
-dofile(areas.modpath.."/interact.lua")
-dofile(areas.modpath.."/legacy.lua")
 dofile(areas.modpath.."/hud.lua")
 
 areas:load()
 
-minetest.register_privilege("areas", {
-	description = "Can administer areas."
-})
-minetest.register_privilege("areas_high_limit", {
-	description = "Can can more, bigger areas."
-})
-
-if not minetest.registered_privileges[areas.config.self_protection_privilege] then
-	minetest.register_privilege(areas.config.self_protection_privilege, {
-		description = "Can protect areas.",
-	})
-end
-
 if minetest.setting_getbool("log_mod") then
 	local diffTime = os.clock() - areas.startTime
-	minetest.log("action", "areas loaded in "..diffTime.."s.")
+	minetest.log("action", "Modified areas mod loaded in "..diffTime.."s.")
 end
 
